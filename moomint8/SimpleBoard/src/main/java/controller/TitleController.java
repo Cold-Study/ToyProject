56355e6195fd1185e07d11bc23b0920c54be56c2
moomint8 <office05053@gmail.com>
2024-01-29 00:34:53 +0900

    refactor(SimpleBoard) : TitleController 에서 메인 페이지 선택
    실제 웹페이지를 구현해서 하는 게 아니다보니 우선 Controller 에서 각 페이지에 맞는 메소드를 호출하도록 구현하려고 합니다.
    실제 각 유저들의 접속마다 새로운 객체가 생성된다 판단하였습니다.
    이에 맞춰 일부 코드를 개선했습니다.

---
+++ moomint8/SimpleBoard/src/main/java/controller/TitleController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import view.TitlePage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TitleController {
+
+    /* 설명. 타이틀 메뉴에서 선택 가능한 숫자 리스트. */
+    private static final ArrayList<Integer> NUMBER_OF_TITLE_SELECTION = new ArrayList<>(List.of(1, 2, 9));
+    public void selectTitleMenu() {
+        TitlePage titlePage = new TitlePage();
+
+        while (true) {
+            try {
+                int input = titlePage.printTitle(NUMBER_OF_TITLE_SELECTION);
+
+                switch (input) {
+                    case 1:
+                        System.out.println("회원 서비스 구현 위치");
+                        break;
+                    case 2:
+                        System.out.println("게시판 서비스 구현 위치");
+                        break;
+                    case 9:
+                        System.out.println("프로그램을 종료합니다...");
+                        return;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
