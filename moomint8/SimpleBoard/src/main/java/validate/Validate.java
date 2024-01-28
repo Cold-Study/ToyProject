9c01ddf184503b06596c25fa2a4fef6133317a50
moomint8 <office05053@gmail.com>
2024-01-29 01:05:56 +0900

    refactor(SimpleBoard) : 메뉴 입력 검증 메소드화
    메뉴 입력에 대한 검증이 중복되어 메소드화했습니다.
    메소드의 재사용을 위한 개선입니다.

---
+++ moomint8/SimpleBoard/src/main/java/validate/Validate.java
@@ -0,0 +1,18 @@
+package validate;
+
+import validate.InputException.NotInListRangeException;
+import validate.InputException.NotNumberException;
+
+import java.util.ArrayList;
+
+public class Validate {
+
+    /* 설명. 메뉴에 대한 Input을 검증하는 메소드입니다. */
+    public static void menu(String input, ArrayList<Integer> numberOfTitleSelection) throws NotNumberException, NotInListRangeException {
+        ValidateInputType validateInputType = new ValidateInputType();
+        validateInputType.isNumber(input);                                  // 숫자가 맞는지 검증
+
+        ValidateInputRange validateInputRange = new ValidateInputRange();
+        validateInputRange.isInNumberList(Integer.parseInt(input), numberOfTitleSelection);   // 리스트 안에 있는 범위 값인지 검증
+    }
+}
--- moomint8/SimpleBoard/src/main/java/view/MemberPage.java
+++ moomint8/SimpleBoard/src/main/java/view/MemberPage.java
@@ -2,6 +2,7 @@ package view;
 
 import validate.InputException.NotInListRangeException;
 import validate.InputException.NotNumberException;
+import validate.Validate;
 import validate.ValidateInputRange;
 import validate.ValidateInputType;
 
@@ -13,7 +14,7 @@ public class MemberPage {
     /* 설명. 회원 페이지입니다. 선택된 메뉴를 반환합니다. */
     public int printMemberMainPage(ArrayList<Integer> numberOfMemberSelection) throws NotNumberException, NotInListRangeException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("============== 게시판 프로그램 ==============");
+        System.out.println("\n============== 회원 페이지 ==============");
         System.out.println("1. 회원 전체 조회");
         System.out.println("2. 회원가입");
         System.out.println("3. 회원 정보 변경");
@@ -24,18 +25,9 @@ public class MemberPage {
 
         String input = sc.nextLine();
 
-        validateMemberMainPageInput(input, numberOfMemberSelection);
+        /* 설명. 회원 페이지의 Input 값이 올바른지 검증하는 메소드입니다. */
+        Validate.menu(input, numberOfMemberSelection);
 
         return Integer.parseInt(input);
     }
-
-    /* 설명. 회원 페이지의 Input 값이 올바른지 검증하는 메소드입니다. */
-    private void validateMemberMainPageInput(String input, ArrayList<Integer> numberOfMemberSelection) throws NotNumberException, NotInListRangeException {
-        ValidateInputType validateInputType = new ValidateInputType();
-        validateInputType.isNumber(input);                                  // 숫자가 맞는지 검증
-
-        ValidateInputRange validateInputRange = new ValidateInputRange();
-        validateInputRange.isInNumberList(Integer.parseInt(input), numberOfMemberSelection);   // 리스트 안에 있는 범위 값인지 검증
-    }
-
 }
