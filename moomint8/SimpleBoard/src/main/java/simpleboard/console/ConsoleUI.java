47808b8409626af11d82df1aee3b1bc2ac9dd257
moomint8 <office05053@gmail.com>
2024-01-29 23:21:42 +0900

    feat(SimpleBoard) : 회원 페이지 구현
    회원 페이지를 구현했습니다. 이제 기능들을 추가할 예정입니다.

--- moomint8/SimpleBoard/src/main/java/simpleboard/console/ConsoleUI.java
+++ moomint8/SimpleBoard/src/main/java/simpleboard/console/ConsoleUI.java
@@ -9,38 +9,83 @@ public class ConsoleUI {
     private final MemberController memberController;
     private final BoardController boardController;
 
+    private final String WRONG_CHOICE_MENU = "존재하지 않는 메뉴입니다. 다시 선택하세요.";
+
     public ConsoleUI(MemberController memberController, BoardController boardController) {
         this.memberController = memberController;
         this.boardController = boardController;
     }
 
     public void start() {
-        selectTitleMenu();
+        showTitleMenu();
     }
 
-    private void selectTitleMenu() {
+    private void showTitleMenu() {
         while (true) {
-            Scanner sc = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
+
             System.out.println("\n============== 게시판 프로그램 ==============");
             System.out.println("1. 회원 서비스");
             System.out.println("2. 게시판 서비스");
             System.out.println("9. 프로그램 종료");
             System.out.print("메뉴를 선택해주세요 : ");
 
-            int input = sc.nextInt();
+            int input = scanner.nextInt();
 
             switch (input) {
                 case 1:     // 회원 페이지로 이동
                     System.out.println("회원 페이지 구현 위치");
+                    showMemberMenu();
                     break;
                 case 2:     // 게시판 페이지로 이동
                     System.out.println("게시판 페이지 구현 위치");
                     break;
                 case 9:     // 프로그램 종료
                     System.out.println("프로그램을 종료합니다...");
+                    scanner.close();
                     return;
                 default:
-                    System.out.println("존재하지 않는 메뉴입니다. 다시 선택하세요.");
+                    System.out.println(WRONG_CHOICE_MENU);
+            }
+        }
+    }
+
+    private void showMemberMenu() {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("\n============== 회원 페이지 ==============");
+            System.out.println("1. 전체 회원 조회");
+            System.out.println("2. 회원가입");
+            System.out.println("3. 로그인");
+            System.out.println("4. 회원 정보 수정");
+            System.out.println("5. 회원 탈퇴");
+            System.out.println("9. 메인 페이지 복귀");
+            System.out.print("메뉴를 선택해주세요 : ");
+
+            int input = scanner.nextInt();
+
+            switch (input) {
+                case 1:     // 회원 전체 조회
+                    System.out.println("전체 회원 조회 구현 위치");
+                    break;
+                case 2:     // 회원가입
+                    System.out.println("회원가입 구현 위치");
+                    break;
+                case 3:     // 회원 정보 변경
+                    System.out.println("로그인 구현 위치");
+                    break;
+                case 4:     // 로그인/로그아웃
+                    System.out.println("회원 정보 수정 구현 위치");
+                    break;
+                case 5:     // 회원 탈퇴
+                    System.out.println("회원 탈퇴 구현 위치");
+                    break;
+                case 9:     // 메인 페이지 복귀
+                    System.out.println("메인 페이지 복귀");
+                    return;
+                default:
+                    System.out.println(WRONG_CHOICE_MENU);
             }
         }
     }
